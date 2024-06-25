26fff6b612cb5588c014f477912373318cfea0ac
zunyong01 <gunnercoyg@khu.ac.kr>
2024-06-25 22:57:18 +0900

    git upload tip

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 print('hello fucking vs')
 print("adadd")
+
+# git init    # 처음 올릴 때만
+# git add . (or any specific file name)
+# git commit -m "commit message"
+# git remote add origin "https://github.com/user.name/repos_name.git"    #origin 추가
+# git remote remove origin    #기존 origin 제거
+# git push -u origin main
